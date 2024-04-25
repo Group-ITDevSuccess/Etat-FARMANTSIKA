5a6ddab052ed37239b496db5d9c9fef670304ea2
TSIDIANY Raharison Muriel <raharisontsidiany@gmail.com>
2024-04-25 13:12:03 +0300

    Mise en place de requet et de script pour formater les nombres et recuper la date du jour

--- utils.py
+++ utils.py
@@ -2,11 +2,15 @@ import os
 import logging
 from datetime import datetime
 
-today = datetime.today().strftime('%d-%m-%Y')
+today = datetime.today()
+
+
+def get_today():
+    return today.strftime('%d/%m/%Y')
 
 
 def write_log(logs, level=None):
-    log_file = os.path.join('logs', f'{today}.log')
+    log_file = os.path.join('logs', f'{today.strftime('%d-%m-%Y')}.log')
 
     logging.basicConfig(filename=log_file, encoding='utf-8', level=level,
                         format='%(asctime)s - %(name)s - %(levelname)s : %(message)s')
@@ -21,3 +25,10 @@ def write_log(logs, level=None):
         logger.critical(logs)
     else:
         logger.exception(logs)
+
+
+def format_number(number):
+    try:
+        return '{:,.2f}'.format(float(number))
+    except ValueError:
+        return number  # Handle non-numeric values gracefully
